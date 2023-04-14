6da7c5489d81229b480c4b36e95dc8770cca0320
Gerald <code@gera2ld.space>
2023-04-14 17:23:58 +0000

    fix: update twind

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,25 +1,32 @@
 import { createApp } from 'vue';
 import 'github-markdown-css';
 import './prism.css';
-import { setup, apply } from 'twind/shim';
-import * as colors from 'twind/colors';
+import { install, injectGlobal } from '@twind/core';
+import presetAutoprefix from '@twind/preset-autoprefix';
+import presetTailwind from '@twind/preset-tailwind';
 import App from './app.vue';
 
-setup({
-  theme: {
-    extend: {
-      colors,
-    },
-  },
-  preflight: (preflight) => ({
-    ...preflight,
-    body: apply`bg(white dark:gray-800) text-gray(800 dark:300)`,
-    a: apply`text-blue(400 hover:600)`,
-    ul: apply`list-disc`,
-    '.markdown-body a.non-existent': apply`text-red-400 cursor-not-allowed`,
-    '.markdown-body a[target=_blank]': apply`underline`,
-  }),
+install({
+  presets: [presetAutoprefix(), presetTailwind()],
 });
+
+injectGlobal`
+body {
+  @apply bg(white dark:gray-800) text-gray(800 dark:300);
+}
+a {
+  @apply text-blue(400 hover:600);
+}
+ul {
+  @apply list-disc;
+}
+.markdown-body a.non-existent {
+  @apply text-red-400 cursor-not-allowed;
+}
+.markdown-body a[target=_blank] {
+  @apply underline;
+}
+`;
 
 const app = createApp(App);
 app.mount(document.body);

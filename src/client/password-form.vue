22d7c52c0157878f3d5680498323778cc0c32026
Gerald <code@gera2ld.space>
2023-10-04 21:09:07 +0800

    fix: focus password input on load

--- src/client/password-form.vue
+++ src/client/password-form.vue
@@ -4,7 +4,7 @@
     class="max-w-sm mt-12 mx-auto p-4 text-center border border-gray-300 dark:border-gray-700"
   >
     <div class="mb-4 text-lg font-bold" v-text="hint"></div>
-    <input type="password" class="mb-4" v-model="password" />
+    <input ref="refInput" type="password" class="mb-4" v-model="password" />
     <div class="flex text-left">
       <div
         class="flex-1 mr-4 text-red-500"
@@ -16,15 +16,20 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, computed } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 import { store } from './util';
 
 const emit = defineEmits<{
   (event: 'submit', payload: string): void;
 }>();
 
+const refInput = ref<HTMLInputElement>();
 const password = ref('');
 const hint = computed(() => store.password?.hint || 'Input password:');
+
+onMounted(() => {
+  refInput.value?.focus();
+});
 
 function handleSubmit() {
   if (!password.value) return;

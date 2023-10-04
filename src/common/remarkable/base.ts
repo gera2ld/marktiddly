1a20622f681a2d9fd43c9b039c861f69d83379f6
Gerald <code@gera2ld.space>
2023-10-04 22:13:08 +0800

    feat: render tags as links

--- src/common/remarkable/base.ts
+++ src/common/remarkable/base.ts
@@ -1,48 +1,26 @@
 import type { Remarkable } from 'remarkable';
 
-export function linkPlugin(
-  md: Remarkable,
-  options?: {
-    resolve?: (path: string) => string;
-  },
-) {
+export function linkPlugin(md: Remarkable) {
   md.core.ruler.before(
     'inline',
     'rel-link',
     (state) => {
       for (const token of state.tokens) {
-        if (token.type === 'inline' && token.content) {
-          // [label](relpath)
-          // [label]([[tiddler-name]])
-          // [[label]]
-          // [[label|tiddler-name]]
-          token.content = token.content.replace(
-            /\[(.+?)\]\((.+?)\)|\[\[(.+?)\]\]/g,
-            (m, g1, g2, g3) => {
-              let label: string;
-              let name: string;
-              let path: string;
-              if (g2) {
-                if (g2.includes('://')) return m;
-                label = g1;
-                if (/^\[\[.*?\]\]$/.test(g2)) {
-                  name = g2.slice(2, -2);
-                } else if (options?.resolve) {
-                  name = options.resolve(g2);
-                  if (!name) path = g2;
-                }
-              } else {
-                [label, name] = g3.split('|');
-                name ||= label.toLowerCase();
-              }
-              path ||= `?p=${encodeURIComponent(name)}`;
-              return `[${label}](${path})`;
-            },
-          );
-          token.content = token.content.replace(
-            /(^|\s)(https?:\/\/\S+)/,
-            '$1<$2>',
-          );
+        let content = token.content as string;
+        if (token.type === 'inline' && content) {
+          // [[label]] -> [label](?p=label)
+          content = content.replace(/\[\[(.+?)\]\]/g, (_, g1) => {
+            const path = `?p=${encodeURIComponent(g1.toLowerCase())}`;
+            return `[${g1}](${path})`;
+          });
+          // auto URL
+          content = content.replace(/(^|\s)(https?:\/\/\S+)/g, '$1<$2>');
+          // #xxx -> [xxx](?p=tags.xxx)
+          content = content.replace(/(^|\s)#([^\s#]+)/g, (_, g1, g2) => {
+            const path = encodeURIComponent(`tags.${g2.toLowerCase()}`);
+            return `${g1}[#${g2}](?p=${path})`;
+          });
+          token.content = content;
         }
       }
       return false;
